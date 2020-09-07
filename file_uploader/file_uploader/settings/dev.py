9c6e91da7f158de2edce673044750d15a7992abd
aless10 <alessio.izzo86@gmail.com>
2020-09-07 12:57:20 +0200

    [fix] fix url issue for downloading

--- file_uploader/file_uploader/settings/dev.py
+++ file_uploader/file_uploader/settings/dev.py
@@ -9,16 +9,16 @@ SECRET_KEY = os.environ.get("DEV_SECRET_KEY")
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DEV_DB_NAME"),
-        'USER': os.environ.get("DEV_DB_USER"),
-        'PASSWORD': os.environ.get("DEV_DB_PASSWORD"),
-        'HOST': os.environ.get("DEV_DB_HOST"),
-        'PORT': os.environ.get("DEV_DB_PORT", 5432)
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get("DEV_DB_NAME"),
+#         'USER': os.environ.get("DEV_DB_USER"),
+#         'PASSWORD': os.environ.get("DEV_DB_PASSWORD"),
+#         'HOST': os.environ.get("DEV_DB_HOST"),
+#         'PORT': os.environ.get("DEV_DB_PORT", 5432)
+#     }
+# }
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
